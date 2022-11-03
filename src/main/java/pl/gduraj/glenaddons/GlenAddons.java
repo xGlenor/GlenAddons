bde7f436d7dae0c2e204ce28708fd081eef75c96
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2022-11-03 14:36:37 +0100

    Initial commit

---
+++ src/main/java/pl/gduraj/glenaddons/GlenAddons.java
@@ -0,0 +1,17 @@
+package pl.gduraj.glenaddons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GlenAddons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
